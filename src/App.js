f16b5aa24b34bacb987f54c4f8a2eabe3740e8fd
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-10 23:34:26 -0500

    added footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navbar/Navbar';
-
 import Home from './components/home/Home';
-
+import Footer from './components/footer/Footer'
 function App() {
 	return (
 		<div className='App'>
 			<NavBar />
 			<Home />
+			<Footer />
 		</div>
 	);
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+function Footer() {
+	return (
+        <footer>
+            <p className='copyright'>&#169; Tonimarie Morris 2020</p>
+        </footer>
+	);
+}
+
+export default Footer;
