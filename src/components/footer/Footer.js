c71bad49eef764b8ca9cf29f01b5e3c3b6f242dd
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-11 02:25:36 -0500

    added contact icons to footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,33 @@
 import React from 'react'
 import './Footer.css'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import {
+	faGithub,
+	faLinkedin,
+	faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 export default function Footer() {
 	return (
-		<footer>
-			<p className='copyright'>&#169; Tonimarie Morris 2020</p>
+		<footer className='footer'>
+			<a
+				href='https://www.linkedin.com/in/tonimarie-morris-50a010104/'
+				target='_blank'
+				rel='noreferrer'>
+				<FontAwesomeIcon icon={faLinkedin} size='2x' color='white' />
+			</a>
+			<a href='https://github.com/tonim99' target='_blank' rel='noreferrer'>
+				<FontAwesomeIcon icon={faGithub} size='2x' color='white' />
+			</a>
+			<a href='mailto:morris.tonimarie@gmail.com'>
+				<FontAwesomeIcon icon={faEnvelope} size='2x' color='white' />
+			</a>
+			<a
+				href='https://twitter.com/TonimarieMorri1'
+				target='_blank'
+				rel='noreferrer'>
+				<FontAwesomeIcon icon={faTwitter} size='2x' color='white' />
+			</a>
 		</footer>
 	);
 }
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,8 @@ import './Home.css'
 export default function Home () {
     return (
         <div className = 'about'>
-            <img className = "top-photo" src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-                alt="coding-pic" />
+            {/* <img className = "top-photo" src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+                alt="coding-pic" /> */}
                 <p className = "statement">Hi! I'm Tonimarie. I'm a Full-Stack Software Engineer living in the beautiful Shenandoah Valley in central Virginia with my
                 two Akitas. I am passionate about making clean and functional web applications. If you like what you see, please feel free to reach out!</p>
             </div>
