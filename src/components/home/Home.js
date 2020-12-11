dc67d4930ffa0992697e0b4f64e706b95ddb01d1
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-11 02:50:33 -0500

    updated introduction

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,10 @@ import './Home.css'
 export default function Home () {
     return (
         <div className = 'about'>
-            {/* <img className = "top-photo" src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
-                alt="coding-pic" /> */}
-                <p className = "statement">Hi! I'm Tonimarie. I'm a Full-Stack Software Engineer living in the beautiful Shenandoah Valley in central Virginia with my
-                two Akitas. I am passionate about making clean and functional web applications. If you like what you see, please feel free to reach out!</p>
+            <p className='intro'>I'm Tonimarie, a full stack Software Engineer with a passion for creating clean, responsive, user-first web and mobile applications. 
+            </p>
+                {/* <p className = "statement">Hi! I'm Tonimarie. I'm a Full-Stack Software Engineer living in the beautiful Shenandoah Valley in central Virginia with my
+                two Akitas. I am passionate about making clean and functional web applications. If you like what you see, please feel free to reach out!</p> */}
             </div>
        
     )
