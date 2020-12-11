6e1544ad56d5e4e498f68cc6e18c4abc0e2263e0
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-11 01:13:58 -0500

    contact styling started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/navbar/Navbar';
 import Home from './components/home/Home';
-import Footer from './components/footer/Footer'
+import Footer from './components/footer/Footer';
 import Contact from './components/Contact';
 function App() {
 	return (
 		<div className='App'>
 			<NavBar />
-			<Home />
-			<Contact />
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/contact'>
+					<Contact />
+				</Route>
+			</Switch>
+
 			<Footer />
 		</div>
 	);
