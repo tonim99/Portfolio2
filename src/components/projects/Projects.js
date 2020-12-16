46a2c5f02c2716e2c1dc1bc74d46c11f2bdbdb07
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-15 19:03:32 -0500

    changed media queries

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Card, Button, CardDeck, Container, Row, Col } from 'react-bootstrap';
+import { Card, Button, Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode, faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import './Projects.scss';
 import Tabletop from 'tabletop';
 export default function Projects() {
 	const [projects, setProjects] = useState([]);
 	useEffect(() => {
 		Tabletop.init({
-			key: '1uwFmEbEq4zu9XBAsp9m57i4uShA2Ug9H9tCpOr8I43U',
+			key: '1d9tAR6r3q7A2SO38rbpBBUalOPZsBfiCq7v8kriYtbM',
 			simpleSheet: true,
 		})
 			.then((data) => setProjects(data))
@@ -14,29 +16,31 @@ export default function Projects() {
 	}, []);
 	return (
 		<Container>
-			{projects.map((project, i) => (
-				<Card key={i}>
-					<Card.Header>{project.Title}</Card.Header>
-					<Card.Body>
-						<Card.Text>{project.Description}</Card.Text>
-						<Card.Img src={project.Image} />
-						<Container>
-							<Row>
-								<Col>
-									<Button onClick={() => console.log('go to repo')}>
-										GitHub Repository
-									</Button>
-								</Col>
-								<Col>
-									<Button onClick={() => console.log('go to project')}>
-										Live Project
-									</Button>
-								</Col>
-							</Row>
-						</Container>
-					</Card.Body>
-				</Card>
-			))}
+			<Row xs={1} sm={1} md={1} lg={4} xl={5}>
+				{projects.map((project, i) => (
+					<Card key={i}>
+						<Card.Header>{project.Title}</Card.Header>
+						<Card.Body>
+							<Card.Text>{project.Description}</Card.Text>
+							<Card.Img src={project.Image} />
+							<Container fluid>
+								<Button onClick={() => console.log('go to repo')}>
+									See Code{' '}
+									<FontAwesomeIcon icon={faCode} size='1x' color='white' />
+								</Button>
+								<Button onClick={() => console.log('go to project')}>
+									Visit Website{' '}
+									<FontAwesomeIcon
+										icon={faExternalLinkAlt}
+										size='1x'
+										color='white'
+									/>
+								</Button>
+							</Container>
+						</Card.Body>
+					</Card>
+				))}
+			</Row>
 		</Container>
 	);
 }
