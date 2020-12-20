d5f6a317dab058696a562e2041e0d7106cd604a0
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-20 02:27:45 -0500

    updated landing page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './Home.scss';
+import image from '../../assets/images/hero-lg.jpg';
 export default function Home() {
 	return (
 		<div className='about'>
-			<p className='statement'>
-				Hi! I'm Tonimarie. I'm a Full-Stack Software Engineer living in the
-				beautiful Shenandoah Valley in central Virginia with my two Akitas. I am
-				passionate about making clean and functional web applications. If you
-				like what you see, please feel free to reach out!
-			</p>
+			<img src={image} alt='coding' className='hero' />
+			<div className='statementDiv'>
+				<p className='statement'>
+					{' '}
+					I am a detail-oriented Full-Stack developer who values collaboration and has an insatiable hunger for knowledge. I create clean, responsive, user-first applications optimized for mobile and web. 
+				</p>
+			</div>
 		</div>
 	);
 }
