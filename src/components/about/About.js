6e5aed45c9b4f7bbfc77a0a1a96b5468eb2f34d7
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-20 03:56:53 -0500

    updated bio

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -6,7 +6,7 @@ export default function About() {
 		<div className='aboutContainer'>
 			<img src={image} alt='profile' className='profile' />
             <h2>About Me</h2>
-            
+            <p>Tonimarie is an account manager turned software engineer. She lives in the beautiful Shenandoah Valley in central Virginia with her two Akitas, and drinks copious amounts of coffee while creating applications for the web. In her spare time, she fronts a band called Blues Soup and a duo called Fourth Street Manor. She also volunteers as the Secretary of the local Democratic Committee.</p>
 		</div>
 	);
 }
