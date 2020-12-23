686d6aeed3eb8f3beb906155979e01a5bcf3fe08
Tonimarie Morris <morris.tonimarie@gmail.com>
2020-12-23 00:22:08 -0500

    cleaned up Home.scss

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -15,19 +15,19 @@ export default function Footer() {
 				href='https://www.linkedin.com/in/tonimarie-morris-50a010104/'
 				target='_blank'
 				rel='noreferrer'>
-				<FontAwesomeIcon icon={faLinkedin} size='2x' color='white' />
+				<FontAwesomeIcon icon={faLinkedin} size='3x' color='white' />
 			</a>
 			<a href='https://github.com/tonim99' target='_blank' rel='noreferrer'>
-				<FontAwesomeIcon icon={faGithub} size='2x' color='white' />
+				<FontAwesomeIcon icon={faGithub} size='3x' color='white' />
 			</a>
 			<a href='mailto:morris.tonimarie@gmail.com'>
-				<FontAwesomeIcon icon={faEnvelope} size='2x' color='white' />
+				<FontAwesomeIcon icon={faEnvelope} size='3x' color='white' />
 			</a>
 			<a
 				href='https://twitter.com/TonimarieMorri1'
 				target='_blank'
 				rel='noreferrer'>
-				<FontAwesomeIcon icon={faTwitter} size='2x' color='white' />
+				<FontAwesomeIcon icon={faTwitter} size='3x' color='white' />
 			</a>
 		</footer>
 	);
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,17 +5,17 @@ import { Link } from 'react-router-dom';
 export default function Home() {
 	return (
 		<header className='header'>
-			<div className='brand-box'>
-				<Link className='link' to='/'>
-					<h1 className='brand'>TM</h1>
+			<div className='header__brand-box'>
+				<Link className='header__link' to='/'>
+					TM
 				</Link>
 			</div>
-			<div className='statement-box'>
+			<div className='header__statement-box'>
 				<h1 className='heading-primary'>
-					<span className='heading-primary-main'>Tonimarie Morris</span>
-					<span class='heading-primary-sub'>Full-Stack Software Engineer</span>
+					<span className='heading-primary--main'>Tonimarie Morris</span>
+					<span class='heading-primary--sub'>Full-Stack Software Engineer</span>
 				</h1>
-				<Link to='/projects' className='btn btn-pink'>
+				<Link to='/projects' className='btn btn--pink btn--animated'>
 					See my work
 				</Link>
 			</div>
