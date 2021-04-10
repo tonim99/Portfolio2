53ab1ee48879791fb72d8e41b607e036b1f4359a
Tonimarie Morris <morris.tonimarie@gmail.com>
2021-04-10 15:44:34 -0400

    added hashrouter

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
 ReactDOM.render(
 	<Router>
 		<React.StrictMode>
-			<App />
+			<HashRouter>
+				<App />
+			</HashRouter>
 		</React.StrictMode>
 	</Router>,
 	document.getElementById('root')
