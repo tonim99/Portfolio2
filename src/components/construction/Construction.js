93241efe128e1003c248bb151789b9e52fa22822
Tonimarie Morris <morris.tonimarie@gmail.com>
2021-09-15 14:51:23 -0400

    started under construction page

---
+++ src/components/construction/Construction.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Construction() {
+    return (
+        <div className="container">
+            <p className="underConstruction">
+                This page is currently under construction. Please check back later. In the meantime, you can view my GitHub <a href="https://github.com/tonim99" className="link">here</a>.
+            </p>
+        </div>
+    )
+}
--- src/components/construction/construction.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Construction() {
-    return (
-        <div className="underConstruction">
-            
-        </div>
-    )
-}
