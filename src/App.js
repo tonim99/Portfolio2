231b1d030fbd5363400bdd5030f0ce57230c005f
Tonimarie Morris <morris.tonimarie@gmail.com>
2021-09-20 16:46:23 -0400

    working with API

--- src/App.js
+++ src/App.js
@@ -10,7 +10,13 @@ import Menu from './components/menu/Menu';
 import About from './components/about/About';
 import NavBar from './components/navbar/Navbar';
 // import Construction from './components/construction/Construction';
-
+const url = 'https://cdn.contentful.com'
+const spaceID = process.env.API_SPACE_ID
+const REACT_APP_API_KEY = `${process.env.REACT_APP_API_KEY}`
+fetch(`${url}/spaces/${spaceID}/entries?access_token=${REACT_APP_API_KEY}&content_type=project`).then((response)=>{
+    console.log(response)
+	// return response.json()
+})
 export default function App() {
 	const [showMenu, setShowMenu] = useState(false);
 	const clickHandler = () => {
